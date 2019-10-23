dd96c5ec0846f2d60b7b825bb5aba24458a7901e
Sahil Khare <56581743+Sahil099@users.noreply.github.com>
2019-10-23 14:37:54 +0530

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -12,42 +12,35 @@ int main()
 
     v1.push_back(5);
 
-    while(v1.back()!=0){
-
+    while(v1.back()!=0)
+    {
         v1.push_back(v1.back()-1);
-
     }
+    
 // end(): Returns an iterator pointing to a position which is NEXT to the last element of the vector. 
 // So sweet of you c++;
 // Address
-    for(it=v1.begin();it!=v1.end();it++){
-
+   
+    for(it=v1.begin();it!=v1.end();it++)
+    {
         cout<<*it<<' ';
-        
-
     }
     cout<<endl ;
 
-    for(int i=0;i<v1.size();i++){
-
-            cout<<v1.at(i)<<' ';
-
-        }
-        cout<<endl ;
-
-    while(!v1.empty()){
-
-        cout<<v1.back()<< ' ';
-        v1.pop_back();
-        
+    for(int i=0;i<v1.size();i++)
+    {
+        cout<<v1.at(i)<<' ';
     }
         cout<<endl ;
 
+    while(!v1.empty())
+    {
+        cout<<v1.back()<< ' ';
+        v1.pop_back();
+    }
+        cout<<endl ;
 
-
-
-return 0;
-
+    return 0;
 }
 
 
